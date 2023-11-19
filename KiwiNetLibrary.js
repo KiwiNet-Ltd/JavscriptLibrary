0efa6913f1db0b0db1eceda4b43a9993919ee061
jglassonnz <72478655+jglassonnz@users.noreply.github.com>
2023-11-19 15:50:34 +1300

    Update KiwiNetLibrary add userguiding check

--- KiwiNetLibrary.js
+++ KiwiNetLibrary.js
@@ -269,14 +269,16 @@ methods.fluxxRenameSave = function(modelID) {
 </script>
     */
     methods.UserGuidingFluxx = function(userprofileID, lastloggedin = true) {
-        if (lastloggedin == "") {
-            window.UGshowintro = true;
+        if (typeof userGuiding == 'undefined') {
+            if (lastloggedin == "") {
+                window.UGshowintro = true;
+            }
+            else {
+                window.UGshowintro = false;
+            }
+            window.granteeType = userprofileID;
+            (function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','triggerNps','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','881222442ID'); 
         }
-        else {
-            window.UGshowintro = false;
-        }
-        window.granteeType = userprofileID;
-        (function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','triggerNps','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','881222442ID'); 
     }
     
     return methods;
